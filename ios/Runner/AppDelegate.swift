17f82a55e55725a05678984af827d13a7db020ed
Tijo Thomas <41670750+tijot123@users.noreply.github.com>
2021-12-27 23:01:10 +0530

    Added Siren for force update in IOS app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,7 @@ import Flutter
 import GoogleMaps
 import Firebase
 import FirebaseAnalytics
+import Siren
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,9 @@ import FirebaseAnalytics
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let siren = Siren.shared
+    siren.rulesManager = RulesManager(globalRules: .critical)
+    siren.wail()
     if #available(iOS 10.0, *) {
           UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
         }
