67a149a2c991fca93b2d326772c298a3ee485d9e
Tijo Thomas <tijo.t@ezetap.com>
2021-12-27 23:20:51 +0530

    Added in app update to android app

--- android/app/src/main/kotlin/com/app/royal_oaks/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/royal_oaks/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.app.royal_oaks
 
+import android.content.Intent
+import android.util.Log
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+private const val MY_UPDATE_REQUEST_CODE = 110011
 
 class MainActivity : FlutterActivity() {
 
@@ -12,5 +18,45 @@ class MainActivity : FlutterActivity() {
         // register plugins
         GeneratedPluginRegistrant.registerWith(flutterEngine)
         //super.configureFlutterEngine(flutterEngine)
+    }
+
+    override fun onResume() {
+        addInAppUpdate()
+        super.onResume()
+    }
+
+    private fun addInAppUpdate() {
+        // Creates instance of the manager.
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        // Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks whether the platform allows the specified type of update,
+        // and current version staleness.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                // Request the update.
+                appUpdateManager.startUpdateFlowForResult(
+                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                    appUpdateInfo,
+                    // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                    AppUpdateType.IMMEDIATE,
+                    // The current activity making the update request.
+                    this,
+                    // Include a request code to later monitor this update request.
+                    MY_UPDATE_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_UPDATE_REQUEST_CODE && resultCode != RESULT_OK) {
+            Log.e("onActivityResult: ", "Failed Update")
+        }
     }
 }
